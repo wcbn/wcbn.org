44ddbe4a5c4995ba94474f27fbe548335b1cc204
DavidTalbot <David.Talbot@target.com>
2020-07-01 00:06:17 -0400

    homepage

--- src/wcbn_core/admin.py
+++ src/wcbn_core/admin.py
@@ -1,1 +1,3 @@
 from django.contrib import admin
+admin.site.site_header = 'wcbn administration'
+admin.site.index_title = 'Freeing your mind, managing your content etc.'
--- src/wcbn_core/views.py
+++ src/wcbn_core/views.py
@@ -1,11 +1,22 @@
-from django.views.generic import TemplateView
+import requests
+from django.views.generic import ListView
+from wcbn_cms.models import Article
+from django.conf import settings
+
+def get_on_air():
+    url = f'{settings.READBACK_URL}/playlist.json'
+    resp = requests.get(url)
+    return resp.json()
 
 
-class IndexView(TemplateView):
-    template_name = 'homepage.html'
-    title = 'Homepage'
+class IndexView(ListView):
+    template_name = 'homepage/homepage.html'
+    model = Article
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        context['title'] = self.title
+        context['title'] = 'Homepage'
+        context['STUDIO_LINE'] = settings.STUDIO_LINE
+        context['RESOURCE_LINKS'] = settings.RESOURCE_LINKS
+        context['on_air'] = get_on_air()['on_air']
         return context
