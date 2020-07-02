5d198a3e4445c45b302a842c3f9906fbf96ed194
DavidTalbot <David.Talbot@target.com>
2020-07-02 15:13:50 -0400

    wip modeling

--- src/wcbn_cms/models.py
+++ src/wcbn_cms/models.py
@@ -17,3 +17,22 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+# class Event(models.Model):
+#     title = models.CharField(max_length=140)
+#     description = models.TextField()
+
+#     start_date = models.DateField()
+#     start_time = models.TimeField(null=True, blank=True)
+#     end_date = models.DateTimeField(null=True, blank=True)
+#     end_time = models.TimeField(null=True, blank=True)
+
+#     location_name = models.CharField(max_length=140, null=True, blank=True)
+#     location_address = models.CharField(max_length=140, null=True, blank=True)
+
+#     url = models.CharField(max_length=140, null=True, blank=True)
+#     age_restriction = models.CharField(max_length=50, null=True, blank=True)
+#     presented_by = models.CharField(max_length=140, null=True, blank=True)
+#     door_time = models.TimeField(null=True, blank=True)
+
