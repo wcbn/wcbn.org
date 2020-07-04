a46da344e9f78cba24593d8d65916d4f2e3b2f64
DavidTalbot <David.Talbot@target.com>
2020-07-04 16:17:06 -0400

    fix bucket name

--- src/app/environments/production.py
+++ src/app/environments/production.py
@@ -25,7 +25,7 @@ CSRF_COOKIE_SECURE = True
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage' 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID') 
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY') 
-AWS_STORAGE_BUCKET_NAME = 'wcbn-dot-org-assets'
+AWS_STORAGE_BUCKET_NAME = 'wcbn-dot-org'
 AWS_DEFAULT_ACL = None
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 
