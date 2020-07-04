ab2e30d9985ba0ad1ffad98846b601c78061d309
DavidTalbot <David.Talbot@target.com>
2020-07-03 22:46:07 -0400

    back to what I had

--- src/static/js/player/AlbumArt.jsx
+++ src/static/js/player/AlbumArt.jsx
@@ -2,44 +2,33 @@ import * as React from "react"
 import { useState, useEffect } from "react"
 import Zoom from "react-medium-image-zoom"
 
-window.handleItunes = (resp) => {
-  console.log("handling" + resp)
-}
-
 function buildItunesURL(song) {
-  console.log("getting itunes url")
-  const { album, artist } = song
   return (
     "https://itunes.apple.com/search?limit=1&entity=album&" +
-    `term=${encodeURIComponent(`${album} ${artist}`)}&` +
-    `callback="handleItunes"`
+    `term=${encodeURIComponent(`${song.album} ${song.artist}`)}`
   )
 }
 
-function AlbumArt(props) {
-  const { song } = props
+function AlbumArt({ song }) {
   const [imageSrc, setImageSrc] = useState(null)
 
   useEffect(() => {
     async function getArtworkURL() {
-      console.log("getting artwork")
       fetch(buildItunesURL(song))
-        // .then((r) => r.json())
-        // .then((data) => {
-        //   const { results } = data
-        //   const [firstResult] = results
-        //   const imageSrc =
-        //     firstResult != null ? firstResult.artworkUrl100 : null
-        //   setImageSrc(imageSrc)
-        // })
+        .then((r) => r.json())
+        .then((data) => {
+          const { results } = data
+          const [firstResult] = results
+          const imageSrc =
+            firstResult != null ? firstResult.artworkUrl100 : null
+          setImageSrc(imageSrc)
+        })
         .catch((reason) => {
           console.log("Cant get album art: " + reason)
         })
     }
     getArtworkURL()
   }, [song.at])
-
-  console.log(imageSrc)
 
   if (imageSrc === null) return null
   return (
