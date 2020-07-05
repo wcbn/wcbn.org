947d8ea85d37d5a9d78c4f6db2c823aca663e007
DavidTalbot <David.Talbot@target.com>
2020-07-05 15:26:13 -0400

    fix ordering

--- src/wcbn_cms/views.py
+++ src/wcbn_cms/views.py
@@ -23,7 +23,8 @@ class EventsListView(ListView):
 class ConcertListView(ListView):
     template_name = "layouts/with_sidebar.html"
     model = Concert
-    ordering = ['-start_date', '-start_time', '-end_date', '-end_time']
+    paginate_by = 20
+    ordering = ['start_date', 'start_time', 'end_date', 'end_time']
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
