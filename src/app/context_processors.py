e3440790c4a2e1b28c97f48138c66a1fade7f9d2
DavidTalbot <David.Talbot@target.com>
2020-07-06 02:06:02 -0400

    add DJANGO_SETTINGS_MODULE

--- src/app/context_processors.py
+++ src/app/context_processors.py
@@ -4,7 +4,7 @@ from django.conf import settings
 def global_config(request):
     return {
         'GA_MEASUREMENT_ID': os.environ.get('GA_MEASUREMENT_ID'),
-        'ENV': os.environ.get('DJANGO_SETTINGS_MODULE'),
+        'ENV': settings.DJANGO_SETTINGS_MODULE,
         'READBACK_URL': settings.READBACK_URL,
         'SOCIAL_ICONS': settings.SOCIAL_ICONS,
         'RESOURCE_LINKS': settings.RESOURCE_LINKS
--- src/app/environments/common.py
+++ src/app/environments/common.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+# dot-delimited path of settings file to use e.g. app.environments.production
+DJANGO_SETTINGS_MODULE = os.environ.get("DJANGO_SETTINGS_MODULE")
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
