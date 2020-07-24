b5098b994dca7960b09fadd9746678d3d61913e3
DavidTalbot <David.Talbot@target.com>
2020-07-24 02:10:00 -0400

    admin tweaks

--- src/wcbn_auth/admin.py
+++ src/wcbn_auth/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
 from .models import User
 
+class UserAdmin(BaseUserAdmin):
+    ordering = ('date_joined',)
+    list_display = ('email', 'first_name', 'last_name', 'is_active', 'date_joined', 'is_staff')
+
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
