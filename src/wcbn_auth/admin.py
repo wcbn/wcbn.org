3700fb8f9d13569f39c81c77b6d0fc9faec4cea4
DavidTalbot <David.Talbot@target.com>
2020-07-24 02:21:08 -0400

    user form

--- src/wcbn_auth/admin.py
+++ src/wcbn_auth/admin.py
@@ -2,10 +2,25 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group
 from .models import User
+from django.utils.translation import gettext, gettext_lazy as _
+
 
 class UserAdmin(BaseUserAdmin):
     ordering = ('date_joined',)
     list_display = ('email', 'first_name', 'last_name', 'is_active', 'date_joined', 'is_staff')
+    fieldsets = (
+        (None, {
+            "fields": (
+                'first_name',
+                'last_name',
+                'email',
+                'password',
+                'is_active',
+                'is_staff',
+                'is_superuser'
+            ),
+        }),
+    )
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
