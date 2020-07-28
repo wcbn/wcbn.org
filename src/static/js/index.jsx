4020cce8737d6860c5ca56fbfc983637a1bbbd39
DavidTalbot <David.Talbot@target.com>
2020-07-27 21:48:25 -0400

    install medium-zoom

--- src/static/js/index.jsx
+++ src/static/js/index.jsx
@@ -1,50 +1,56 @@
+import mediumZoom from 'medium-zoom'
+
 // turbolinks
-var Turbolinks = require("turbolinks");
-Turbolinks.start();
+var Turbolinks = require('turbolinks')
+Turbolinks.start()
 
 // google analytics
 window.subscribeGTAG = (GA_MEASUREMENT_ID) => {
-  window.dataLayer = window.dataLayer || [];
+  window.dataLayer = window.dataLayer || []
   function gtag() {
-    dataLayer.push(arguments);
+    dataLayer.push(arguments)
   }
-  gtag("js", new Date());
+  gtag('js', new Date())
 
-  document.addEventListener("turbolinks:load", (event) => {
-    if (typeof gtag == "function") {
-      gtag("config", GA_MEASUREMENT_ID, {
+  document.addEventListener('turbolinks:load', (event) => {
+    if (typeof gtag == 'function') {
+      gtag('config', GA_MEASUREMENT_ID, {
         page_title: event.target.title,
         page_path: event.data.url.replace(
           window.location.protocol +
-            "//" +
+            '//' +
             window.location.hostname +
-            (location.port && ":" + location.port),
-          ""
+            (location.port && ':' + location.port),
+          ''
         ),
-      });
+      })
     }
-  });
-};
+  })
+}
+
+document.addEventListener('turbolinks:load', function () {
+  mediumZoom('[data-zoomable]')
+})
 
 // TODO only add listener in dev
 // django debug toolbar
-document.addEventListener("turbolinks:load", (event) => {
-  if ("djdt" in window) {
+document.addEventListener('turbolinks:load', (event) => {
+  if ('djdt' in window) {
     setTimeout(() => {
-      window.djdt.init();
-    }, 1000); //race condition? #hack
+      window.djdt.init()
+    }, 1000) //race condition? #hack
   }
-});
+})
 
 // jquery
-import jQuery from "jquery";
-window.$ = window.jQuery = jQuery;
+import jQuery from 'jquery'
+window.$ = window.jQuery = jQuery
 
 // global since recaptcha injects itself before anything else *rolls eyes*
 window.enableFormSubmit = () => {
   document
     .querySelector("[data-callback='enableFormSubmit']")
-    .closest("form")
+    .closest('form')
     .querySelector('input[type="submit"]')
-    .removeAttribute("disabled");
-};
+    .removeAttribute('disabled')
+}
